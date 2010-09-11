04b8a7b7123cb48c72f94f2dbec9946f59ef6d3f
Alec <alecda@gmail.com>
2010-09-11 13:33:56 +0100

    added the title section into index.php and made a usernames array

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 ﻿<?php
+//will eventually be put into functions.php
 
 $con mysql_connect('localhost','a_site','');
 $db = mysql_select_db('sals');
 
+//get list of users into array
 
+$sql=mysql_query("SELECT * FROM users");
+$usernames=mysql_fetch_assoc($sql));
+$sql=NULL;
+echo"<html><head>";
+//get title for head
+if(!$_GET['p'] || $_GET['p']=1){
+	$sql=mysql_query("SELECT title FROM content WHERE id ='1'");
+	$title=mysql_fetch_assoc($sql);
+	$sql=NULL;
+}else{
+	$sql=mysql_query("SELECT title FROM content WHERE id='".$_GET['p']."'");
+	$title=mysql_fetch_assoc($sql);
+	$sql=NULL;
+}
+echo"<title>".$title." | site</title>";
 ?>
